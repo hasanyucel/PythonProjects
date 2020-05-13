e16666e689b9a48fe98e402b8a454de4b23e1c8d
hasanyucel34 <hasanyucel34@gmail.com>
2020-05-13 11:05:30 +0300

    Basic exercise added.

---
+++ Basics/Basic-010.py
@@ -0,0 +1,6 @@
+# Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn. 
+# Sample value of n is 5
+# Expected Result : 615
+
+n = input("Please enter an integer : ")
+print(int(n)+int(n+n)+int(n+n+n))
