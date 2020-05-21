dfcd35945b17dfefb8848f9b34b51ea734a0dfcc
hasanyucel34 <hasanyucel34@gmail.com>
2020-05-21 10:07:53 +0300

    Basic exercise added.

---
+++ Basics/Basic-037.py
@@ -0,0 +1,6 @@
+# Write a Python program to display your details like name, age, address in three different lines.
+
+def display_details(name,age,address):
+    print("Name\t:",name,"\nAge\t:",age,"\nAddress\t:",address)
+
+print(display_details("Hasan",25,"Kayseri"))
