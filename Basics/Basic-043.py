660d7025aeffaa01c55969f4d42640d9a12d0190
hasanyucel34 <hasanyucel34@gmail.com>
2020-05-23 11:57:20 +0300

    Basic exercise added.

---
+++ Basics/Basic-043.py
@@ -0,0 +1,9 @@
+# Write a Python program to get OS name, platform and release information.
+
+import os 
+import sys
+import platform
+
+print(os.name)
+print(sys.platform)
+print(platform.release())
