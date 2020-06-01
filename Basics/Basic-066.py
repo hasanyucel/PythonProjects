20c10ed9575302fa2b01b5cd76ab6d6176eceff0
hasanyucel34 <hasanyucel34@gmail.com>
2020-06-01 09:08:19 +0300

    Basic exercise added.

---
+++ Basics/Basic-066.py
@@ -0,0 +1,6 @@
+# Write a Python program to calculate body mass index.
+
+weight = float(input("Enter Your Weight: "))
+height = float(input("Enter Your Height: "))
+
+print("Your BMI: ",weight/(pow(height,2)),"")
