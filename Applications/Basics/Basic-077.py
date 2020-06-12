3de8888c5701dad2daf60a41999b7e33f6a83617
hasanyucel34 <hasanyucel34@gmail.com>
2020-06-12 08:17:04 +0300

    Basic exercise added.

---
+++ Applications/Basics/Basic-077.py
@@ -0,0 +1,9 @@
+# Write a Python program to test whether the system is a big-endian platform or little-endian platform.
+
+import sys
+if sys.byteorder == "little":
+    #intel, alpha
+    print("Little-endian platform.")
+else:
+    #motorola, sparc
+    print("Big-endian platform.")
