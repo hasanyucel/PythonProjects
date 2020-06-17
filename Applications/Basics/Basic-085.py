9a495f11a493f583678f46a4e4be7934d625e207
hasanyucel34 <hasanyucel34@gmail.com>
2020-06-17 08:04:16 +0300

    Basic exercise added.

---
+++ Applications/Basics/Basic-085.py
@@ -0,0 +1,10 @@
+# Write a Python program to check whether a file path is a file or a directory.
+
+import os  
+path="__pycache__"  
+if os.path.isdir(path):  
+    print("\nIt is a directory")  
+elif os.path.isfile(path):  
+    print("\nIt is a normal file")  
+else:  
+    print("It is a special file (socket, FIFO, device file)" )
