059f0a68d0843fbbf0c974659f006310e50fd116
hasanyucel <hasanyucel34@gmail.com>
2020-06-18 22:31:34 +0300

    Github Follow Bot Minor Changes.

--- Applications/Projects/Github Bot/README.md
+++ Applications/Projects/Github Bot/README.md
@@ -1,4 +1,4 @@
-# GitHub-Follow-Bot
+# GitHub Follow Bot
 
 A bot done with python to follow users in GitHub.
 
@@ -10,11 +10,4 @@ A bot done with python to follow users in GitHub.
 - Then run the python file (It need selenium to run, so `pip install selenium`).
 
 
-## Why I did this?
 
-Somewhere, I read:
-- One in ten people you follow will follow you back.
-- One in hundred people you follow will star your repos.
-- One in thousand people you follow will fork your repos.
-
-Might be true, might be not 🤷‍.
--- Applications/Projects/Github Bot/follow_bot.py
+++ Applications/Projects/Github Bot/follow_bot.py
@@ -4,51 +4,55 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from user_credentials import user, passw
 
-# Initializing the headless chrome
 driver = webdriver.Chrome()
-driver.get("https://github.com/login")
-wait = WebDriverWait(driver, 10)
+followers = []
+# Initializing the headless chrome with given website
+def login_github(website):
 
-# Locating username and password field
-username = wait.until(EC.presence_of_element_located((By.ID, "login_field")))
-password = wait.until(EC.presence_of_element_located((By.ID, "password")))
+    driver.get(website)
+    wait = WebDriverWait(driver, 10)
+    # Locating username and password field
+    username = wait.until(EC.presence_of_element_located((By.ID, "login_field")))
+    password = wait.until(EC.presence_of_element_located((By.ID, "password")))
+    # password and username need to go into these values
+    username.send_keys(user)
+    password.send_keys(passw)
+    # Clicking the sign in button
+    login_form = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@value='Sign in']")))
+    login_form.click()
 
-# password and username need to go into these values
-username.send_keys("username")
-password.send_keys("password")
-
-# Clicking the sign in button
-login_form = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@value='Sign in']")))
-login_form.click()
-
-# Go to the followers tab
-nameOfUser = "" # Input github username here
-
-driver.get("https://github.com/{}?tab=followers".format(nameOfUser))
-time.sleep(3)
+# Go to the followers tab of given username
+def go_followers_page_of_user(nameOfUser):
+    driver.get("https://github.com/{}?tab=followers".format(nameOfUser))
+    time.sleep(3)
 
 # Find the followers
-users = driver.find_elements_by_xpath("//a[@data-hovercard-type='user']")
-temp = []
-for follower in users:
-    temp.append(follower.get_attribute("href"))
-list_set = set(temp) 
-followers = (list(list_set))
+def find_followers_of_user():
+    users = driver.find_elements_by_xpath("//a[@data-hovercard-type='user']")
+    temp = []
+    for follower in users:
+        temp.append(follower.get_attribute("href"))
+    list_set = set(temp) 
+    followers = (list(list_set))
+    # Follow everyone who is following 'nameOfUser'
+    for user in followers:
+        for page in range(1, 5):
+            string = "{}?page={}&tab=following".format(user, page)
+            driver.get(string)
+            time.sleep(3)
 
-# Follow everyone who is following 'nameOfUser'
-for user in followers:
-    for page in range(1, 5):
-        string = "{}?page={}&tab=following".format(user, page)
-        driver.get(string)
-        time.sleep(3)
+            follow_button = driver.find_elements_by_xpath("//input[@aria-label='Follow this person']")
 
-        follow_button = driver.find_elements_by_xpath("//input[@aria-label='Follow this person']")
+            try:
+                for i in follow_button:
+                    i.submit()
+            except:
+                pass
 
-        try:
-            for i in follow_button:
-                i.submit()
-        except:
-            pass
+    driver.close()
 
-driver.close()
+login_github("https://github.com/login")
+go_followers_page_of_user("torvalds") #Enter a user to follow the followings of the users
+find_followers_of_user()
