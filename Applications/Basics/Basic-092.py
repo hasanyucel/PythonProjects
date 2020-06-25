4cd84e4fc310b3ba0003e552be9545d766f2f250
hasanyucel <hasanyucel34@gmail.com>
2020-06-25 07:52:33 +0300

    Basic exercise added.

---
+++ Applications/Basics/Basic-092.py
@@ -0,0 +1,8 @@
+# Write a Python program to define a string containing special characters in various forms.
+
+print("\#{'}${\"}@/")
+print("\#{'}${"'"'"}@/")
+print(r"""\#{'}${"}@/""")
+print('\#{\'}${"}@/')
+print('\#{'"'"'}${"}@/')
+print(r'''\#{'}${"}@/''')
