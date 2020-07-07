930822ff84ab63515ff430cbaba8eff4e2e6de50
hasanyucel <hasanyucel34@gmail.com>
2020-07-07 08:42:23 +0300

    Rastgele mahalle oluşturma fonksiyonu eklendi.

--- Applications/Projects/Sipariş/musteri_olustur.py
+++ Applications/Projects/Sipariş/musteri_olustur.py
@@ -1,19 +1,31 @@
 import random
 
 def satir_getir(yol):
-    dosya = open(yol,"r",encoding="utf8")
+    dosya = open(yol,"r",encoding="utf8",errors='ignore')
     lines = dosya.readlines()
     rand_line = random.randint(0,len(lines)-1)
     satir = (lines[rand_line])
     dosya.close()
-    return satir
+    return satir.replace("\n","")
 
 def numara_olustur():
     numara = random.randint(1000000,9999999)
     return ("0352"+str(numara))
 
-ad = satir_getir("isimler.txt").replace("\n","")
-soyad = satir_getir("soyisimler.txt").replace("\n","")
+def adres_olustur():
+    lst=["MELİKGAZİ","KOCASİNAN"]
+    mah = random.choice(lst)
+    path=""
+    if(mah=="MELİKGAZİ"):
+        path = "melikgazi_mahalleleri.txt"
+    elif(mah=="KOCASİNAN"):
+        path = "kocasinan_mahalleleri.txt"
+    return mah+"/"+satir_getir(path).replace("\n","")
+
+ad = satir_getir("isimler.txt")
+soyad = satir_getir("soyisimler.txt")
 numara = numara_olustur()
+adres = adres_olustur()
 print(ad,soyad)
 print(numara)   
+print(adres)
