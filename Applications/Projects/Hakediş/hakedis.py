6f979943c88bf92ecdd188974f363363494f45a2
hasanyucel <hasanyucel34@gmail.com>
2020-07-11 11:41:40 +0300

    Hakedişler için giriş fonksiyonları oluşturuldu.

---
+++ Applications/Projects/Hakediş/hakedis.py
@@ -0,0 +1,37 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from keyboard import press
+
+driver = webdriver.Chrome()
+
+def login_gspn(website):
+    driver.get(website)
+    Alert(driver).accept()
+    wait = WebDriverWait(driver, 10)
+    username = wait.until(EC.presence_of_element_located((By.XPATH, """//*[@id="login_form_all"]/div[1]/dl/dd[1]/input""")))
+    password = wait.until(EC.presence_of_element_located((By.XPATH, """//*[@id="login_form_all"]/div[1]/dl/dd[2]/input""")))
+    usercr = get_username_password()
+    username.send_keys(usercr[0])
+    password.send_keys(usercr[1])
+    # login_form = wait.until(EC.presence_of_element_located((By.XPATH, """//*[@id="login_form_all"]/div[1]/img""")))
+    # login_form.click()
+    # time.sleep(10)
+    # press('enter')
+
+def go_management_page():   
+    wait = WebDriverWait(driver, 10)
+    management = wait.until(EC.presence_of_element_located((By.XPATH, """//*[@id="MAIN_04"]/span""")))
+    management.click()
+
+def get_username_password():
+    dosya = open("D:\\user2.txt","r")
+    line = dosya.readline() 
+    return line.split(",")
+
+login_gspn("https://gspn1.samsungcsportal.com/")
+go_management_page()
