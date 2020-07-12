857d28e8ce8f819d4a2d5768d518f2b1c7c78291
hasanyucel <hasanyucel34@gmail.com>
2020-07-12 22:21:22 +0300

    Fonksiyon düzenlemesi yapıldı.

--- Applications/Projects/Hakediş/hakedis.py
+++ Applications/Projects/Hakediş/hakedis.py
@@ -9,11 +9,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from keyboard import press
 
 driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 10)
 
 def login_gspn(website):
     driver.get(website)
     Alert(driver).accept()
-    wait = WebDriverWait(driver, 10)
     username = wait.until(EC.presence_of_element_located((By.XPATH, """//*[@id="login_form_all"]/div[1]/dl/dd[1]/input""")))
     password = wait.until(EC.presence_of_element_located((By.XPATH, """//*[@id="login_form_all"]/div[1]/dl/dd[2]/input""")))
     usercr = get_username_password()
@@ -22,16 +22,19 @@ def login_gspn(website):
     login_form = wait.until(EC.presence_of_element_located((By.XPATH, """//*[@id="login_form_all"]/div[1]/img""")))
     login_form.click()
     Alert(driver).accept()
+
+def go_management():
     wait.until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"""/html/frameset/frame[2]""")))
     management = wait.until(EC.element_to_be_clickable((By.XPATH, """//*[@id="MAIN_04"]/span""")))
     management.click()
+
+def go_work_order():
     driver.switch_to_default_content()
     wait.until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"""/html/frameset/frame[3]""")))
     wait.until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"""//*[@id="leftMenus"]""")))
     wait.until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"""//*[@id="b2BLeftMenuScroll"]""")))
     st = wait.until(EC.presence_of_element_located((By.XPATH, """/html/body/table/tbody/tr[2]/td/table/tbody/tr[4]/td""")))
     st.click()
-
 
 
 def get_username_password():
@@ -41,3 +44,5 @@ def get_username_password():
     return line.split(",")
 
 login_gspn("https://gspn1.samsungcsportal.com/")
+go_management()
+go_work_order()
