87d9a5ea3777121efce210d0e46b44933d15eff5
hasanyucel <hasanyucel34@gmail.com>
2020-07-20 08:11:28 +0300

    Basic exercise added.

---
+++ Applications/Basics/Basic-112.py
@@ -0,0 +1,14 @@
+# Write a Python program to remove the first item from a specified list.
+
+lst = [12,15,6,5,3,8]
+
+lst.pop(0)
+
+print(lst)
+
+#w3sources answer
+color = ["Red", "Black", "Green", "White", "Orange"]
+print("\nOriginal Color: ",color)
+del color[0]
+print("After removing the first color: ",color)
+print()
