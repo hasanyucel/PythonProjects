47f09f6bcfa813186eec571503016d94ea125260
hasanyucel <hasanyucel34@gmail.com>
2020-07-23 08:55:27 +0300

    Basic exercise added.

---
+++ Applications/Basics/Basic-119.py
@@ -0,0 +1,5 @@
+#  Write a Python program to display a floating number in specified numbers.
+
+order_amt = 212.375056987
+print('\nThe total order amount comes to %f' % order_amt)
+print('The total order amount comes to %.2f' % order_amt)
