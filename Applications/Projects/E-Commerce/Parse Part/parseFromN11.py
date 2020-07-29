9bf66686a5dc6c910d0fae017b8c592bc06dde4b
hasanyucel <hasanyucel34@gmail.com>
2020-07-29 09:30:44 +0300

    Parse example added.

---
+++ Applications/Projects/E-Commerce/Parse Part/parseFromN11.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+class Part:
+    def __init__(self,link):
+        r = requests.get(link)
+        source = BeautifulSoup(r.content,"lxml")
+        self.partName = source.select('h1.proName')[0].text.strip()
+        self.partTitle = source.select('h2.proSubName')[0].text.strip()
+        self.cost = (source.select('div.newPrice')[0].find('ins')).text.strip()
+
---
+++ Applications/Projects/E-Commerce/Parse Part/test.py
@@ -0,0 +1,5 @@
+from parseFromN11 import Part
+
+p1 = Part('https://urun.n11.com/uzaktan-kumanda/samsung-k-serisi-kumanda-orjinal-bn59-01259b-P232444387')
+
+print(p1.partName,p1.partTitle,p1.cost)
