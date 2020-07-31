9db6137a05fcfe8061f07c556370cb28458d124d
hasanyucel <hasanyucel34@gmail.com>
2020-07-31 17:31:46 +0300

    Basic exercise added.

--- Applications/Projects/E-Commerce/Parse Part/test.py
+++ Applications/Projects/E-Commerce/Parse Part/test.py
@@ -1,6 +1,6 @@
 from parseFromN11 import ParseProduct
 
 p1 = ParseProduct('https://urun.n11.com/uzaktan-kumanda/samsung-tv-kumandasi-orjinal-bn59-01242a-suhd-akilli-kumanda-P238001173')
-
-print(p1.partName,p1.partTitle,p1.partCost)
-print(p1.partDetails)
+print(p1.partName)
+print(p1.partTitle)
+print(p1.partCost)
