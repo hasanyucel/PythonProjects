ef65f1b37e9b5ebb85433add868774580588c5d6
hasanyucel <hasanyucel34@gmail.com>
2020-08-05 08:50:04 +0300

    Sudoku has been completed.

--- Applications/Projects/Sudoku Solver/solve.py
+++ Applications/Projects/Sudoku Solver/solve.py
@@ -1,15 +1,3 @@
-import numpy as np
-
-sudoku = np.array([ [7, 8, 0, 4, 0, 0, 1, 2, 0], 
-                    [6, 0, 0, 0, 7, 5, 0, 0, 9], 
-                    [0, 0, 0, 6, 0, 1, 0, 7, 8], 
-                    [0, 0, 7, 0, 4, 0, 2, 6, 0], 
-                    [0, 0, 1, 0, 5, 0, 9, 3, 0], 
-                    [9, 0, 4, 0, 6, 0, 0, 0, 5], 
-                    [0, 7, 0, 3, 0, 0, 0, 1, 2], 
-                    [1, 2, 0, 0, 0, 7, 4, 0, 0], 
-                    [0, 4, 9, 2, 0, 6, 0, 0, 7]  ])
-
 def print_sudoku(sudoku):
     for i in range(len(sudoku)):
         if i % 3 == 0 and i != 0:
@@ -24,4 +12,51 @@ def print_sudoku(sudoku):
             else:
                 print(str(sudoku[i][j]) + " ", end="")
 
-print_sudoku(sudoku)
+def find_empty(sudoku):
+    for i in range(len(sudoku)):
+        for j in range(len(sudoku[0])):
+            if sudoku[i][j] == 0:
+                return (i, j)  # row, col
+
+    return None
+
+def valid(sudoku, num, pos):
+    # Check row
+    for i in range(len(sudoku[0])):
+        if sudoku[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(sudoku)):
+        if sudoku[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check sudokux
+    sudokux_x = pos[1] // 3
+    sudokux_y = pos[0] // 3
+
+    for i in range(sudokux_y*3, sudokux_y*3 + 3):
+        for j in range(sudokux_x * 3, sudokux_x*3 + 3):
+            if sudoku[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
+def solve(sudoku):
+    find = find_empty(sudoku)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(sudoku, i, (row, col)):
+            sudoku[row][col] = i
+
+            if solve(sudoku):
+                return True
+
+            sudoku[row][col] = 0
+
+    return False
+
