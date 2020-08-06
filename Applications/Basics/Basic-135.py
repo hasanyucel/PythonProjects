e99aff78f39e8128394e50388e203cf78c17ef5e
hasanyucel <hasanyucel34@gmail.com>
2020-08-06 13:44:05 +0300

    Basic exercise added.

---
+++ Applications/Basics/Basic-135.py
@@ -0,0 +1,9 @@
+# Write a Python program to print a variable without spaces between values. 
+# Sample value : x =30
+# Expected output : Value of x is "30"
+
+x = 30
+print('Value of x is "'+str(x)+'"')
+
+x = 30
+print('Value of x is "{}"'.format(x))
