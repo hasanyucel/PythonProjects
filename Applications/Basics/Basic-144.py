445890a60d5252fb72cf48d8758dff14bf581628
hasanyucel <hasanyucel34@gmail.com>
2020-08-12 08:07:51 +0300

    Basic exercise added.

---
+++ Applications/Basics/Basic-144.py
@@ -0,0 +1,5 @@
+# Write a Python program to check whether variable is of integer or string. 
+
+print(isinstance(25,int) or isinstance(25,str))
+print(isinstance([25],int) or isinstance([25],str))
+print(isinstance("25",int) or isinstance("25",str))
