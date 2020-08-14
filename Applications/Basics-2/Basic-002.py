6bdf1a32e8c4b027d96f3589a89dfaa7830e9364
hasanyucel <hasanyucel34@gmail.com>
2020-08-14 08:02:02 +0300

    Basic exercise added.

---
+++ Applications/Basics-2/Basic-002.py
@@ -0,0 +1,6 @@
+# Write a Python program to create all possible strings by using 'a', 'e', 'i', 'o', 'u'. Use the characters exactly once.
+
+import random
+char_list = ['a','e','i','o','u']
+random.shuffle(char_list)
+print(''.join(char_list))
