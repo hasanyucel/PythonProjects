e28d1ad887fccee1f90e5aaa0379467892d26111
hasanyucel <hasanyucel34@gmail.com>
2020-08-24 08:48:35 +0300

    Teste GSPN açma modülü entegre edildi.

--- Applications/Projects/Sipariş/test.py
+++ Applications/Projects/Sipariş/test.py
@@ -1,5 +1,6 @@
 import sip_no_olustur as sno
 import musteri_olustur as mo
+import opengspn as og
 
 sip_no = sno.siparis_no_olustur()
 m_adi = mo.musteri_ad_olustur()
@@ -7,3 +8,7 @@ m_soyadi = mo.musteri_soyad_olustur()
 m_tel = mo.numara_olustur()
 m_adres = mo.adres_olustur()
 model = mo.model_bul("BN59-01220D")
+
+
+# og.login_gspn("https://gspn1.samsungcsportal.com/")
+# og.go_management()
