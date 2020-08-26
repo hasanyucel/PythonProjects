652f1101c8336fdd7b2ef6a0a3a7a2f282bf4051
hasanyucel <hasanyucel34@gmail.com>
2020-08-26 20:53:09 +0300

    Captchanın üzerindeki çizgi belirlendi.

--- Applications/Projects/Captcha Solver/captcha_solve.py
+++ Applications/Projects/Captcha Solver/captcha_solve.py
@@ -1,24 +1,15 @@
-# Python program to illustrate HoughLine 
-# method for line detection 
-from cv2 import cv2 
-import numpy as np 
-  
-# Reading the required image in  
-# which operations are to be done.  
-# Make sure that the image is in the same  
-# directory in which this python program is 
-img = cv2.imread('3.png') 
-  
-# Convert the img to grayscale 
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
-cv2.imshow("",gray)
-cv2.waitKey(0)
-# Apply edge detection method on the image 
-edges = cv2.Canny(gray,1000,350,apertureSize = 3) 
+import numpy as np
+from cv2 import cv2
 
-# This returns an array of r and theta values 
-lines = cv2.HoughLines(edges,1,np.pi/180, 200) 
-    
-cv2.imshow("",edges)
+image = cv2.imread('2.png')
+cv2.imshow("image",image)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+cv2.imshow("gri",gray)
+
+cizgi = cv2.threshold(gray, 40, 255, cv2.THRESH_BINARY_INV)[1]
+cv2.imshow("cizgi",cizgi)
+
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
