924df8adf5c165aab97a5b49eeb98e769b17198f
hasanyucel <hasanyucel34@gmail.com>
2020-08-26 08:19:15 +0300

    OCR'a captcha testi yapıldı.

--- Applications/Projects/OCR/test.py
+++ Applications/Projects/OCR/test.py
@@ -3,6 +3,6 @@ from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
 
-a=pytesseract.image_to_string(Image.open('t4.png'))
+a=pytesseract.image_to_string(Image.open('20.png'))
 
 print(a)
