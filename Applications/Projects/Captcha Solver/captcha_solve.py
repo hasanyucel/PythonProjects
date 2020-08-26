190247a20fdefb372ddbcf6640dabe3488849fc2
hasanyucel <hasanyucel34@gmail.com>
2020-08-26 11:19:38 +0300

    Test applied

--- Applications/Projects/Captcha Solver/captcha_solve.py
+++ Applications/Projects/Captcha Solver/captcha_solve.py
@@ -1,13 +1,24 @@
-import pandas as pd
-import numpy as np
-from cv2 import cv2
-import glob
-import imutils
-from imutils import paths
-import os
-import os.path
+# Python program to illustrate HoughLine 
+# method for line detection 
+from cv2 import cv2 
+import numpy as np 
+  
+# Reading the required image in  
+# which operations are to be done.  
+# Make sure that the image is in the same  
+# directory in which this python program is 
+img = cv2.imread('3.png') 
+  
+# Convert the img to grayscale 
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
+cv2.imshow("",gray)
+cv2.waitKey(0)
+# Apply edge detection method on the image 
+edges = cv2.Canny(gray,1000,350,apertureSize = 3) 
 
-captcha_image = 'aaw7e.png'
-# Load the image and convert it to grayscale
-image = cv2.imread(captcha_image)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# This returns an array of r and theta values 
+lines = cv2.HoughLines(edges,1,np.pi/180, 200) 
+    
+cv2.imshow("",edges)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
