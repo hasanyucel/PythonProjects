c9de92c38e8e9398e48c87e9fe2dfc4ff0f754a3
hasanyucel <hasanyucel34@gmail.com>
2020-08-27 10:20:02 +0300

    Düzeltmeler yapıldı. Yapılacaklar eklendi.

--- Applications/Projects/Captcha Solver/captcha_solve.py
+++ Applications/Projects/Captcha Solver/captcha_solve.py
@@ -7,8 +7,14 @@ cv2.imshow("image",image)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
 cv2.imshow("gri",gray)
 
-cizgi = cv2.threshold(gray, 40, 255, cv2.THRESH_BINARY_INV)[1]
+cizgi = cv2.threshold(gray, 40, 200, cv2.THRESH_BINARY_INV)[1]
 cv2.imshow("cizgi",cizgi)
+
+res = gray+cizgi
+cv2.imshow("res",res)
+
+other = cv2.threshold(res, 60, 255, cv2.THRESH_BINARY_INV)[1]
+cv2.imshow("other",other)
 
 
 cv2.waitKey(0)
