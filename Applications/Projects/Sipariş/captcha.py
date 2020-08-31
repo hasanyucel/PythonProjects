d188f61599093dea18abc831fbcfa3cff36bfe66
hasanyucel <hasanyucel34@gmail.com>
2020-08-31 15:46:07 +0300

    Captcha kütüphanesi test edildi.

--- Applications/Projects/Sipariş/captcha.py
+++ Applications/Projects/Sipariş/captcha.py
@@ -3,10 +3,13 @@ solver = TwoCaptcha('62bb86c9bba1329fb3e04e9d126c3071')
 
 def captcha_solve(path):
     result = solver.normal(path)
-    return result
+    return result.get("code")
 
 def get_balance():
     balance = solver.balance()
     return balance
 
+# print(get_balance())
+# a = captcha_solve("30.png")
+# print(a)
 # print(get_balance())
