b00d36249c909b86dd51d41cc06a635f1534fca1
hasanyucel <hasanyucel34@gmail.com>
2020-08-31 14:03:02 +0300

    Modül düzenlendi.

--- Applications/Projects/Sipariş/captcha.py
+++ Applications/Projects/Sipariş/captcha.py
@@ -2,9 +2,11 @@ from twocaptcha import TwoCaptcha
 solver = TwoCaptcha('62bb86c9bba1329fb3e04e9d126c3071')
 
 def captch_solve(path):
-    result = solver.normal('captchas/2.png')
+    result = solver.normal(path)
     return result
 
 def get_balance():
     balance = solver.balance()
     return balance
+
+# print(get_balance())
