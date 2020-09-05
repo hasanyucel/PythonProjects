a28d660b42472f27c86bb2500b8844181e7c28a3
hasanyucel <hasanyucel34@gmail.com>
2020-09-05 22:14:34 +0300

    Program yavaşlatıldı.

--- Applications/Projects/Sipariş/main.py
+++ Applications/Projects/Sipariş/main.py
@@ -5,14 +5,20 @@ import time
 def start():
     k.kontrol()
     sa.gspn_giris("https://gspn1.samsungcsportal.com/")
+    time.sleep(5)
     sa.yonetime_git()
+    time.sleep(5)
     with open("siparisler.txt") as dosya: 
         for satir in dosya:
             sa.is_emri_olustur()
             sa.sip_no_doldur()
+            time.sleep(5)
             sa.musteri_ekle()
+            time.sleep(30)
             sa.urun_bilgilerini_gir(satir.strip())
+            time.sleep(30)
             sa.parca_ekle_ve_iste(satir.strip())
+            time.sleep(15)
     # sa.tarayici_kapat()
 
 start()
