12f97be2a0944a1201dbd0f7a1f5034724f0694f
hasanyucel <hasanyucel34@gmail.com>
2020-09-09 15:47:39 +0300

    Sipariş no oluşturma güncellendi.

--- Applications/Projects/Sipariş/sip_no_olustur.py
+++ Applications/Projects/Sipariş/sip_no_olustur.py
@@ -25,14 +25,14 @@ def siparis_no_olustur():
     # yoksa tarihi ve 1 sayisi insert et return 1
     if row is None:
         imlec.execute('INSERT INTO counter VALUES(?,?)',(trh,1))
-        sonuc = trh+"MS01"
+        sonuc = trh+"HS01"
     # varsa sip_sayisinin değişkenini al bir arttır update et return sip_sayisi+1
     else:
         imlec.execute("SELECT sip_sayisi FROM counter WHERE tarih=?", [trh])
         kayitlar = imlec.fetchone()
         gecici_degisken = kayitlar[0] + 1
         imlec.execute("UPDATE counter set sip_sayisi = ? where tarih = ?", (gecici_degisken,trh))
-        sonuc = trh+"MS"+str("{:02d}".format(gecici_degisken))
+        sonuc = trh+"HS"+str("{:02d}".format(gecici_degisken))
     db.commit()
     db.close()
     return sonuc
