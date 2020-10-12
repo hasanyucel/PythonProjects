374f07bcd01952124058a46f35a4671815f212b6
hasanyucel <hasanyucel34@gmail.com>
2020-10-12 14:18:43 +0300

    Sipariş geçildi.

---
+++ Applications/Projects/E-Commerce/GG-Api/test.py
@@ -0,0 +1,16 @@
+from GGAPI import GGAPI
+api = GGAPI.GGAPI('x','y','z','w')
+
+cargoService = api.getCargoService()
+productService = api.getProductService()
+saleService = api.getSaleService()
+storeService = api.getStoreService()
+userMessageService = api.getUserMessageService()
+
+"""
+print(dir(<serviceObject (like cargoService)>))
+print(saleService.getActiveSales())
+"""
+
+satis = saleService.getActiveSales()
+print(satis)
