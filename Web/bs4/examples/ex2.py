b2cf5c3fd211ad957b119a055c7b2871e80cab5c
hasanyucel <hasanyucel34@gmail.com>
2021-07-14 09:39:23 +0300

    Trendyol veri çekme tamamlandı.

--- Web/bs4/examples/ex2.py
+++ Web/bs4/examples/ex2.py
@@ -3,19 +3,18 @@ import sqlite3
 from bs4 import BeautifulSoup
 
 db = sqlite3.connect('veritabani.sqlite')
-im = db.cursor()
-#im.execute("CREATE TABLE urunler (id, urun, fiyat)")
+cursor = db.cursor()
+#cursor.execute("CREATE TABLE products (id, product, price)")
 url = 'https://www.trendyol.com/cep-telefonu-x-c103498'
-html_text = requests.get(url,params={'q': 'pi:5'}).text
-soup = BeautifulSoup(html_text, 'lxml')
-urunler = soup.find_all("div", {"class": "p-card-wrppr"})
-for urun in urunler:
-    urun_id = urun['data-id']
-    urun_adi = urun.find("div", {"class": "prdct-desc-cntnr-ttl-w two-line-text"}).find("span",{"class": "prdct-desc-cntnr-name"})["title"]
-    fiyat = urun.find_all("div", {"class": "prc-box-sllng"})[0].text
-    im.execute("INSERT INTO urunler VALUES (?,?,?)", (urun_id,urun_adi,fiyat))
-    print(urun_id,urun_adi,fiyat)
-db.commit()
+for i in range(100):
+    html_text = requests.get(url,params={'pi': str(i)}).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    products = soup.find_all("div", {"class": "p-card-wrppr"})
+    for product in products:
+        product_id = product['data-id']
+        product_name = product.find("div", {"class": "prdct-desc-cntnr-ttl-w two-line-text"}).find("span",{"class": "prdct-desc-cntnr-name"})["title"]
+        price = product.find_all("div", {"class": "prc-box-sllng"})[0].text
+        cursor.execute("INSERT INTO products VALUES (?,?,?)", (product_id,product_name,price))
+        #print(product_id,product_name,price)
+    db.commit()
 db.close()
-
-
