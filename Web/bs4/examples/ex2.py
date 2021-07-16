e09b8a582253cc2edd50231ce737a63f0a93a7e6
hasanyucel <hasanyucel34@gmail.com>
2021-07-16 14:19:32 +0300

    Ürünleri veritabanına çekme tamamlandı.

--- Web/bs4/examples/ex2.py
+++ Web/bs4/examples/ex2.py
@@ -1,20 +1,25 @@
 import requests
 import sqlite3
 from bs4 import BeautifulSoup
+import timeit
 
 db = sqlite3.connect('veritabani.sqlite')
 cursor = db.cursor()
-#cursor.execute("CREATE TABLE products (id, product, price)")
-url = 'https://www.trendyol.com/cep-telefonu-x-c103498'
-for i in range(100):
+#cursor.execute("CREATE TABLE products (id, product, link)")
+url = 'https://www.trendyol.com/sr?wb=794&wc=110304'
+start = timeit.default_timer()
+for i in range(40):
     html_text = requests.get(url,params={'pi': str(i)}).text
     soup = BeautifulSoup(html_text, 'lxml')
     products = soup.find_all("div", {"class": "p-card-wrppr"})
     for product in products:
         product_id = product['data-id']
         product_name = product.find("div", {"class": "prdct-desc-cntnr-ttl-w two-line-text"}).find("span",{"class": "prdct-desc-cntnr-name"})["title"]
-        price = product.find_all("div", {"class": "prc-box-sllng"})[0].text
-        cursor.execute("INSERT INTO products VALUES (?,?,?)", (product_id,product_name,price))
-        #print(product_id,product_name,price)
+        link = product.find('a', href=True)
+        link = "https://www.trendyol.com"+link["href"]
+        cursor.execute("INSERT INTO products VALUES (?,?,?)", (product_id,product_name,link))
+        #print(product_id,product_name,link)
     db.commit()
 db.close()
+stop = timeit.default_timer()
+print('Time: ', stop - start) 
