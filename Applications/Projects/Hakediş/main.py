7965e1af6768931e9091d8230ddd4115da3c2f88
HASAN HÜSEYİN YÜCEL <hasanyucel34@gmail.com>
2022-09-11 17:12:09 +0300

    Update main.py

--- Applications/Projects/Hakediş/main.py
+++ Applications/Projects/Hakediş/main.py
@@ -1,6 +1,6 @@
 from hakedis import login_gspn,go_management,read_data_from_txt
 
 
-login_gspn("https://gspn1.samsungcsportal.com/")
+login_gspn("https://portal.com/")
 go_management()
 read_data_from_txt('hakedisnote.txt')
